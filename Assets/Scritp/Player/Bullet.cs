d2c3265b2950ef2515ec28884ab56bc9284af557
AJRV26 <ajrvjarinee17@gmail.com>
2023-10-06 17:48:11 -0500

    Player & Bullet

---
+++ Assets/Scritp/Player/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Bullet")]
+    private Rigidbody2D rb2D;
+    public int damage = 2; 
+    public float speedB;
+     
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {  
+       transform.Translate(Vector3.up * speedB * Time.deltaTime);
+       Destroy(gameObject, 3f);
+    }
+
+}
